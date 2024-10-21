0914c6c542d103c89b8321e0c9cdcc91f49cddc5
Juan Gonon <juanfegonon27@gmail.com>
2024-10-21 15:53:43 -0600

    AuhService

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,16 +1,22 @@
 import { Request, Response } from "express";
 import { RegisterUserDto } from "../../domain";
+import { AuthService } from "../services/auth.service";
 
 export class AuthController{
 
     // DI
-    constructor(){}
+    constructor(
+        public readonly authService: AuthService
+    ){}
 
     register = (req : Request, res: Response) => {
         const [error, registerDto] = RegisterUserDto.create(req.body)
         if( error ) return res.status(400).json({ error })
+            
+        this.authService.registerUser(registerDto!)
+        .then((user) => res.json(user) )
 
-        res.json(registerDto)
+
     }
     loginUser = (req : Request, res: Response) => {
         res.json('Login user')
