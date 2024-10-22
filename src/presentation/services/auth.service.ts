4c0322916ef742ccf5cc33d849d5d99fd64e94e5
Juan Gonon <juanfegonon27@gmail.com>
2024-10-22 16:20:04 -0600

    link de verificacion

--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { bcryptAdapter, JwtAdapter } from "../../config";
+import { bcryptAdapter, envs, JwtAdapter } from "../../config";
 import { UserModel } from "../../data";
 import {
     CustomError,
@@ -6,10 +6,13 @@ import {
     UserEntity,
     LoginUserDTO,
 } from "../../domain";
+import { EmailService } from "./email.service";
 
 export class AuthService {
     //DI
-    constructor() {}
+    constructor(
+        private readonly emailService: EmailService
+    ) {}
 
     public async registerUser(registerUserDto: RegisterUserDto) {
         const existUser = await UserModel.findOne({
@@ -22,6 +25,9 @@ export class AuthService {
 
             user.password = bcryptAdapter.hash(registerUserDto.password);
             await user.save();
+
+            await this.sendEmailValidationLink(user.email)
+
             const { password, ...userEntity } = UserEntity.fromObject(user);
             const token = await JwtAdapter.generateToken({ id: user.id })
 
@@ -60,5 +66,31 @@ export class AuthService {
         } catch (error) {
             throw CustomError.internalServer(`${error}`);
         }
+    }
+
+    private sendEmailValidationLink = async(email: string) => {
+        const token = await JwtAdapter.generateToken({ email })
+
+        if(!token) throw CustomError.internalServer('Error getting token')
+
+        const link = `${envs.WEBSERVER_URL}/auth/validate-email/${token}`;
+
+        const html = `
+        <h1> Validate your email </h1>
+        <p> Click on the following link to validate your email </p>
+        <a href="${link}" >Validate your email: ${email} </a>
+        `
+
+        const options = {
+            to: email,
+            subject: 'Validate your email',
+            htmlBody: html
+        }
+
+        const isSent = await this.emailService.sendEmail(options)
+
+        if(!isSent) throw CustomError.internalServer('Error sending email')
+
+        return true
     }
 }
