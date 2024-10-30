e9fe46e52efbfa4c2ef95815f8b038365f5daeb5
Juan Gonon <juanfegonon27@gmail.com>
2024-10-30 15:16:32 -0600

    routes category

---
+++ src/presentation/category/controller.ts
@@ -0,0 +1,24 @@
+import { CustomError } from "../../domain";
+import { Request, Response } from "express";
+
+export class CategoryController {
+    //DI
+    constructor() {}
+
+    private handleError = (error: unknown, res: Response) => {
+        if (error instanceof CustomError) {
+            return res.status(error.statusCode).json({ error: error.message });
+        }
+        return res.status(500).json({ error: "Internal server" });
+    };
+
+    createCategory = async (req: Request, res: Response) => {
+
+      res.json('Create Category')
+    };
+
+    getCategories = async (req: Request, res: Response) => {
+
+      res.json('Get Category')
+    };
+}
