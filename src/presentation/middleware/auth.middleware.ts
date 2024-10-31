284d1d122a542d789ba6c818f7cda755c3748776
Juan Gonon <juanfegonon27@gmail.com>
2024-10-31 14:49:29 -0600

    AuthMiddleware validar token

--- src/presentation/middleware/auth.middleware.ts
+++ src/presentation/middleware/auth.middleware.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { JwtAdapter } from "../../config";
+import { UserModel } from "../../data";
+import { UserEntity } from "../../domain";
 
 export class AuthMiddleware{
 
@@ -13,6 +15,16 @@ export class AuthMiddleware{
 
       try {
          const payload = await JwtAdapter.validateToken<{ id: string}>(token)
+         if(!payload) return res.status(401).json({error: 'Invalid token' })
+
+         const user = await UserModel.findById(payload.id)
+         if(!user) return res.status(401).json({ error: 'Invalid token - user '})
+         
+            // todo: validar si el usuario esta activo
+
+         req.body.user = UserEntity.fromObject(user)
+
+         next()
          
       } catch (error) {
          console.log(error)
