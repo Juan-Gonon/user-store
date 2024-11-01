fcdef70713b2d8e5426d85daf4b02c9e95cdcaf4
Juan Gonon <juanfegonon27@gmail.com>
2024-10-31 19:25:39 -0600

    Pagination DTO and query

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,4 +1,4 @@
-import { CreateCategoryDto, CustomError } from "../../domain";
+import { CreateCategoryDto, CustomError, PaginationDto } from "../../domain";
 import { Request, Response } from "express";
 import { CategoryService } from "../services/category.service";
 
@@ -25,9 +25,16 @@ export class CategoryController {
     };
 
     getCategories = async (req: Request, res: Response) => {
-        this.categoryService
-            .getCategories()
-            .then((categories) => res.json(categories))
-            .catch((error) => this.handleError(error, res));
+
+      const  { page = 1, limit = 10  } = req.query
+      const [ error, paginationDto] = PaginationDto.create(+page, +limit)
+
+      if (error) return res.status(400).json({ error })
+
+      res.json(paginationDto)
+        // this.categoryService
+        //     .getCategories()
+        //     .then((categories) => res.json(categories))
+        //     .catch((error) => this.handleError(error, res));
     };
 }
