75ecde2adff7b50a70f801bdb697f91e47e9f0cc
Juan Gonon <juanfegonon27@gmail.com>
2024-11-04 18:50:25 -0600

    crear semilla para poblar BD

--- src/data/mongo/mongo-database.ts
+++ src/data/mongo/mongo-database.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+import mongoose, { mongo } from "mongoose"
 
 interface Options {
     mongoUrl: string
@@ -21,5 +21,9 @@ export class MongoDatabase {
             console.log('Mongo connection error')
             throw error
         }
+    }
+
+    static async disconnect () {
+        await mongoose.disconnect()
     }
 }
