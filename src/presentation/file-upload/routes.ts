7d50583e3826fa2d2d6628a418a8b59794ec1dfa
Juan Gonon <juanfegonon27@gmail.com>
2024-11-05 20:25:27 -0600

    mover el archivo a su destino permanente

--- src/presentation/file-upload/routes.ts
+++ src/presentation/file-upload/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { FileUploadController } from "./controller";
+import { FileUploadService } from "../services/file-upload.service";
 
 export class FileUploadRoutes {
     static get routes(): Router {
         const router = Router();
-        const controller = new FileUploadController()
+        const service = new FileUploadService()
+        const controller = new FileUploadController(service)
 
         // api/upload/single/ user | category | product 
         // api/upload/multiple/ user | category | product 
--- src/presentation/services/file-upload.service.ts
+++ src/presentation/services/file-upload.service.ts
@@ -1,12 +1,29 @@
+import { UploadedFile } from "express-fileupload";
+import path from "path";
+import fs from 'fs'
+
 export class FileUploadService {
     // DI
     constructor() {}
 
     private checkFolder(folderPath: string) {
-        throw new Error("Not implemented");
+        if(!fs.existsSync(folderPath)){
+         fs.mkdirSync(folderPath)
+        }
     }
 
-    public uploadSingle(file: any, folder: string = 'uploads', validExtensions: string[] = ['png', 'jpg', 'jpeg', 'gif' ]) {}
+    public async uploadSingle(file: UploadedFile, folder: string = 'uploads', validExtensions: string[] = ['png', 'jpg', 'jpeg', 'gif' ]) {
+      try {
+         const fileExtension = file.mimetype.split('/').at(1)
+         const destination = path.resolve(__dirname, '../../../', folder)
+         this.checkFolder(destination)
 
-    public uploadMultiple(file: any, folder: string = 'uploads', validExtensions: string[] = ['png', 'jpg', 'jpeg', 'gif' ]) {}
+         file.mv(destination + `/mi-imagen.${fileExtension}`)
+         
+      } catch (error) {
+         console.log(error)
+      }
+    }
+
+    public async uploadMultiple(file: UploadedFile, folder: string = 'uploads', validExtensions: string[] = ['png', 'jpg', 'jpeg', 'gif' ]) {}
 }
