f4b0eeb44f4f125a8d198d9712fe8436cbe8557f
Juan Gonon <juanfegonon27@gmail.com>
2024-11-08 18:23:12 -0600

    Middleware de verificacion de archivo

--- src/presentation/file-upload/routes.ts
+++ src/presentation/file-upload/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { FileUploadController } from "./controller";
 import { FileUploadService } from "../services/file-upload.service";
+import { FileUploadMiddleware } from "../middleware/file-upload.middleware";
 
 export class FileUploadRoutes {
     static get routes(): Router {
         const router = Router();
         const service = new FileUploadService()
         const controller = new FileUploadController(service)
+
+        router.use(FileUploadMiddleware.containFiles)
 
         // api/upload/single/ user | category | product 
         // api/upload/multiple/ user | category | product 
